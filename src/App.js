03dc51f7ef42086baa0b1e8464122614d6f18203
robertandreiboboc <andreiboboccibo@gmail.com>
2024-02-04 20:41:07 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from "./menu/menu";
+
+function App() {
+  return (
+    <div className="App">
+       <Menu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/menu/menu.js
@@ -0,0 +1,55 @@
+// Inside YourMenuComponent.js
+import React, { useState } from 'react';
+import './menu.css';
+import ro_img from "../assets/romania.png";
+import de_img from "../assets/german.png";
+import menu_hamburger from "../assets/menu.png";
+
+const YourMenuComponent = () => {
+    const [isMobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+    return (
+        <div className={`menu-container ${isMobileMenuVisible ? 'active' : ''}`}>
+            <div className="home-button">
+                <p style={{ color: "#C1A267" }}>Consult - </p>
+                <p style={{ paddingLeft: "3px" }}> Juridic.</p>
+            </div>
+
+            {/* Desktop View */}
+            <div className="desktop-menu">
+                <div className="main-category">Despre Noi</div>
+                <div className="main-category">Consultanta</div>
+                <div className="main-category">Servicii</div>
+                <div className="main-category">Contact</div>
+            </div>
+
+            {/* Mobile View */}
+            <div className="mobile-menu">
+                <div
+                    className={`menu-toggle-button ${isMobileMenuVisible ? 'active' : ''}`}
+                    onClick={() => setMobileMenuVisible(!isMobileMenuVisible)}
+                >
+                    <img className="menu-hamburger" src={menu_hamburger} alt="menu icon" />
+                </div>
+                <div className="mobile-categories">
+                    <div className="main-category1">Home</div>
+                    <div className="dec-line"></div>
+                    <div className="main-category">Despre Noi</div>
+                    <div className="main-category">Consultanta</div>
+                    <div className="main-category">Servicii</div>
+                    <div className="main-category">Contact</div>
+                    <div className="menu-lng">
+                        <div className="main-category-img">
+                            <img className="img-ro" src={ro_img} alt="Romanian flag" />
+                        </div>
+                        <div className="main-category-img1">
+                            <img className="img-de" src={de_img} alt="German flag" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default YourMenuComponent;

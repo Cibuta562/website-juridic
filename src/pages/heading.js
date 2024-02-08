6cbe863a262d8b72017956de4051e797053dcff4
robertandreiboboc <andreiboboccibo@gmail.com>
2024-02-08 19:35:19 +0200

    heading

--- src/pages/heading.js
+++ src/pages/heading.js
@@ -1,10 +1,73 @@
 import "./heading.css"
 import heading from "../assets/heading.jpg"
+import upArrow from "../assets/arrow.png"
 
 function Heading() {
+
+    function moveToTop() {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+
+
     return(
+        <div>
         <div className="heading-cont">
-           <img className="img-heading" src={heading }/>
+            <h1 className="heading-quote">
+                UN DREPT NEDOVEDIT ESTE
+                PRECUM UN DREPT INEXISTENT
+            </h1>
+
+            <div>
+                <button className="btn-heading">
+                    Consultanta Online >
+                </button>
+            </div>
+
+                <button className="btn-top" onClick={moveToTop}>
+                    <img className="img-arrow" src={upArrow} alt={"btn-top"}/>
+                </button>
+
+        </div>
+
+            <div className="stats-column">
+                <div className="stats-row">
+                    <p className="heading-stats-number">
+                        121
+                    </p>
+                    <p className="heading-stats-title">
+                        Colaboratori externi
+                    </p>
+                </div>
+                <div className="stats-row">
+                    <p className="heading-stats-number">
+                        779
+                    </p>
+                    <p className="heading-stats-title">
+                        Clienti
+                    </p>
+                </div>
+                <div className="stats-row">
+                    <p className="heading-stats-number">
+                        2750
+                    </p>
+                    <p className="heading-stats-title">
+                        Cauze Instrumentate
+                    </p>
+                </div>
+                <div className="stats-row" style={{borderBottom: "1px solid white"}}>
+                    <p className="heading-stats-number">
+                        547
+                    </p>
+                    <p className="heading-stats-title">
+                        Seminarii si Stagii
+                    </p>
+                </div>
+
+            </div>
         </div>
     )
 }

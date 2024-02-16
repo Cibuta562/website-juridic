6adb77022afedd4cabe6e5c60a7bf4d6c3709b4b
robertandreiboboc <andreiboboccibo@gmail.com>
2024-02-16 20:03:17 +0200

    img added

--- src/pages/servicii.js
+++ src/pages/servicii.js
@@ -1,9 +1,18 @@
 import "./servicii.css"
 import penal from "../assets/heading.jpg"
+import briefImg from "../assets/brief-img.jpg";
+import servicii1 from "../assets/servicii1.jpg";
+import servicii2 from "../assets/servicii2.jpg";
+import servicii3 from "../assets/servicii3.jpg";
+import servicii4 from "../assets/servicii4.jpg";
+import servicii5 from "../assets/servicii5.jpg";
+import servicii6 from "../assets/servicii6.jpg";
+import briefDown from "../assets/brief-down.jpg";
 
 
 const Servicii = () => {
     return(
+        <div>
         <div className="servicii-cont">
             <p className="servicii-title">
                  Servicii Juridice
@@ -15,7 +24,7 @@ const Servicii = () => {
             <div className="row-servicii">
                 <div className="col-servicii">
                     <div>
-                        <img src={penal} alt="img penal" className="img-serv"/>
+                        <img src={servicii1} alt="img penal" className="img-serv"/>
                     </div>
                     <div className="text-servicii">
                         <p className="heading-servicii"> Drept Penal </p>
@@ -28,7 +37,7 @@ const Servicii = () => {
                 </div>
                 <div className="col-servicii">
                     <div>
-                        <img src={penal} alt="img penal" className="img-serv"/>
+                        <img src={servicii2} alt="img penal" className="img-serv"/>
                     </div>
                     <div className="text-servicii">
                         <p className="heading-servicii"> Drept Civil </p>
@@ -41,7 +50,7 @@ const Servicii = () => {
                 </div>
                 <div className="col-servicii">
                     <div>
-                        <img src={penal} alt="img penal" className="img-serv"/>
+                        <img src={servicii3} alt="img penal" className="img-serv"/>
                     </div>
                     <div className="text-servicii">
                         <p className="heading-servicii"> Drept Rutier </p>
@@ -56,7 +65,7 @@ const Servicii = () => {
             <div className="row-servicii1">
                 <div className="col-servicii">
                     <div>
-                        <img src={penal} alt="img penal" className="img-serv"/>
+                        <img src={servicii4} alt="img penal" className="img-serv"/>
                     </div>
                     <div className="text-servicii">
                         <p className="heading-servicii"> Declaratii Fiscale </p>
@@ -69,7 +78,7 @@ const Servicii = () => {
                 </div>
                 <div className="col-servicii">
                     <div>
-                        <img src={penal} alt="img penal" className="img-serv"/>
+                        <img src={servicii5} alt="img penal" className="img-serv"/>
                     </div>
                     <div className="text-servicii">
                         <p className="heading-servicii"> Dreptul Familiei </p>
@@ -82,18 +91,44 @@ const Servicii = () => {
                 </div>
                 <div className="col-servicii">
                     <div>
-                        <img src={penal} alt="img penal" className="img-serv"/>
+                        <img src={servicii6} alt="img penal" className="img-serv"/>
                     </div>
                     <div className="text-servicii">
                         <p className="heading-servicii"> Dreptul Muncii </p>
                         <p className="desc-servicii"> detasari pe teritoriul UE, formulare A1, contracte de munca internationale, recuperari salarii,
-                            munca pe teritoriul german fara forme legale </p>
+                            munca fara forme legale </p>
                         <button className="btn-servicii">
                             Mai multe detalii >
                         </button>
                     </div>
                 </div>
             </div>
+
+            <div className="line-dec-ver2"></div>
+
+            <div className="brief-servicii">
+                <div className="second-brief-servicii">
+                    <div className="div-servicii" >
+                        <p className="brief-heading">Cum te putem ajuta</p>
+                        <div className="line-dec"></div>
+                        <div className="brief-text">
+                            <p>Daca ati primit un formular de la o institutie germana, cum ar fi: Finante (Finazamt), Primarie (Rathaus), Politie (Polizeiamt), Inregistrare firme (Gewerbeamt) si nu stiti sa-l completati sau unde sa-l trimiteti, va putem ajuta noi. Ne veti putea trimite documentul primit impreuna cu datele de identificare iar noi il vom completa. Ulterior veti primi pe adresa dumneavoastra de mail documentul impreuna cu indrumarile necesare.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="first-brief-servicii">
+                    <img className="brief-img-servicii" src={briefDown}/>
+                </div>
+            </div>
+
+
+        </div>
+
+            <div>
+
+            </div>
+
         </div>
     )
 }

f9b66aea52e87c301098cf98feec076f9bef2f3e
robertandreiboboc <andreiboboccibo@gmail.com>
2024-03-29 11:39:23 +0200

    soon page added and solved form desktop view

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Navigate, Route, Routes, useNavigate} from "react-router-dom";
+import Despre from "./pages/despreNoi";
+import Consultanta from "./pages/consultanta";
+import Payment from "./pages/formPayment";
+import Terms from "./pages/terms";
+import Soon from "./pages/soon";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const Root = () => {
+    const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+    const login = () => {
+        setIsAuthenticated(true);
+    };
+
+    // const logout = () => {
+    //     setIsAuthenticated(false);
+    // };
+
+    return (
+        <React.StrictMode>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/despre/noi" element={<Despre/>} />
+                    <Route path="/consultanta" element={<Consultanta />} />
+                    <Route path="/termeni/si/conditii" element={<Terms />} />
+                    {/*<Route path="/admin" element={<Admin />} />*/}
+                    {/*<Route path="https://cibuta562.github.io/secure/payment/completed" element={<Payment />} />*/}
+                    <Route path="/secure/payment/completed" element={<Payment />} />
+                    <Route path="/website-juridic" element={<App />} />
+                    <Route path="/" element={<Soon />} />
+                </Routes>
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Root />);
+
+reportWebVitals();
---
+++ src/pages/soon.js
@@ -0,0 +1,26 @@
+import "./soon.css";
+
+const Soon = () => {
+    return(
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                textAlign: 'center'
+            }}
+        >
+        <div className="soon-cont">
+            <p className="soon-p">
+                Pagina este in constructie!
+            </p>
+            <p className="soon-p">
+                Revenim in cel mai scurt timp cu un nou website!
+            </p>
+        </div>
+        </div>
+    )
+}
+
+export default Soon
